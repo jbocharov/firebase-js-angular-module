3c8a43c5bd1f2ba5da9a01e03a474e3c89a41c36
John Bocharov <john.bocharov@gmail.com>
2014-02-17 07:45:30 -0800

    yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('firebaseJsAngularModuleApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
